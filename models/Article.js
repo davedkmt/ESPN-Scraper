65a48f91db11109348e2f2fb741596b8278e7fa3
davemtadesse <31293749+davemtadesse@users.noreply.github.com>
2017-12-05 22:34:01 -0600

    complete

---
+++ models/Article.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true
+  },
+  // `subtitle` is required and of type String
+subtitle: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
